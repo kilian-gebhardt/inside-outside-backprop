27c2195874b21058de1e4c72a75805f5cae18227
Kilian Gebhardt <kilian.gebhardt@tu-dresden.de>
2017-10-18 10:04:52 +0200

    dynet EM script

--- io_dynet.py
+++ io_dynet.py
@@ -1,13 +1,15 @@
 import dynet as dy
 from itertools import product
 
+# lengths of words of the form a^n
 corpus = [1, 3, 5, 2]
 
 # rules
 # 0: S -> a
 # 1: S -> S S
 
-def build_hypergraph_rec(i, graph):
+# function that builds the hypergraph from length
+def build_hypergraph_rec(i, graph={}):
   if i not in graph: 
     if i == 1:
       graph[i] = [(0, [])]
@@ -18,18 +20,15 @@ def build_hypergraph_rec(i, graph):
         build_hypergraph_rec(i-j, graph)
         inputs.append((1, [j, i-j]))
       graph[i] = inputs
+  return graph
 
-        
-m = dy.ParameterCollection()
-initial_values = [0.2, 0.8]
-p = {}
-for idx, val in enumerate(initial_values):
-  p[idx] = m.add_parameters((1), init=dy.ConstInitializer(val))
-trainer = dy.AdamTrainer(m, alpha=0.01)
-
-def build_network(p, graph):
+# converts rule probabilies and hypergraph into dynet computation network
+def build_network(p_, graph):
+  dy.renew_cg()
+  
+  p = {i: dy.parameter(p_[i]) for i in p_}
   nodes = sorted([n for n in graph])
-  output = nodes[-1]
+  
   network_nodes = {}
   for n in nodes:
     s = dy.zeros(dim=1)
@@ -39,26 +38,27 @@ def build_network(p, graph):
         prod = prod * network_nodes[child]
       s = s + prod
     network_nodes[n] = s
-  return network_nodes, output
-  
-def create_network(p_, graph):
-  dy.renew_cg()
-  p = {}
-  for i in p_:
-    p[i] = dy.parameter(p_[i])
-  return build_network(p, graph)
-  
+  return network_nodes, nodes[-1] 
 
-for i in range(3):
+m = dy.ParameterCollection()
+initial_values = [0.2, 0.8]
+p = {}
+for idx, val in enumerate(initial_values):
+  p[idx] = m.add_parameters((1), init=dy.ConstInitializer(val))
+trainer = dy.AdamTrainer(m, alpha=0.01)
+
+for i in range(1,4):
+  print("\nSTART of Epoch", i, "\n")
   counts = {}
   for idx in p:
-    print(dy.parameter(p[idx]).value())
+    print("rule", idx, "prob:", dy.parameter(p[idx]).value())
     counts[idx] = 0.0
+  print()
+  
   for elem in corpus:
-    graph = {} 
-    build_hypergraph_rec(elem, graph)
-    print(graph)
-    network, output = create_network(p, graph)
+    hypergraph = build_hypergraph_rec(elem, {})
+    print("hypergraph for", elem, ":", hypergraph)
+    network, output = build_network(p, hypergraph)
     loss = 1 * network[output]
     loss.backward()
     for n in range(1, output + 1):
@@ -68,9 +68,9 @@ for i in range(3):
       count = (rv.gradient() * rv.value() / loss.value())[0]
       print("rule", r, "prob", rv.value(), "gradient", rv.gradient(), "count", count)
       counts[r] += count
-    
     print()
-  print("Total counts", counts)
+  
+  print("Total counts of epoch", i, ":", counts)
   c_sum = sum([counts[r] for r in counts])
   for r in counts:
     p[r] = m.add_parameters(1, init=dy.ConstInitializer(counts[r] / c_sum))
@@ -78,4 +78,3 @@ for i in range(3):
 print("Final rule weights")
 for idx in p:
   print(idx, dy.parameter(p[idx]).value())
-     
